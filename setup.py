cc1f78b8aef2296646b41ed1a09228326244088c
Christopher Rabotin <christopher@sparrho.com>
2015-03-09 11:25:47 +0000

    Bumped version 0.1.0.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from os.path import join, dirname
 from setuptools import setup, find_packages
 
-VERSION = (0, 0, 11)
+VERSION = (0, 1, 0)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
