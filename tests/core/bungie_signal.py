2c140c38b1255f54fc352111048d983cecb7e559
Christopher Rabotin <christopher@sparrho.com>
2015-06-24 11:22:57 +0100

    Signal processor test is performed by using the default processor but in a custom signal processor. Kills two birds with one stone.

--- tests/core/bungie_signal.py
+++ tests/core/bungie_signal.py
@@ -1,41 +1,20 @@
-from _collections import defaultdict
-
-from bungiesearch import Bungiesearch
-from bungiesearch.utils import update_index, delete_index_item
+'''
+This test signal acts as a proxy to BungieSignalProcessor. It allows us
+to test the functionality of the default signal processor while using a
+custom processor instead, hence testing that we can plug in and use a custom
+signal processor.
+'''
+from bungiesearch.signals import BungieSignalProcessor
 from django.db.models import signals
 
 
-__items_to_be_indexed__ = defaultdict(list)
-class BungieTestSignalProcessor(object):
+class BungieTestSignalProcessor(BungieSignalProcessor):
 
     def handle_save(self, sender, instance, **kwargs):
-        try:
-            Bungiesearch.get_index(sender, via_class=True)
-        except KeyError:
-            return # This model is not managed by Bungiesearch.
-
-        try:
-            buffer_size = Bungiesearch.BUNGIE['SIGNALS']['BUFFER_SIZE']
-        except KeyError:
-            buffer_size = 100
-
-        __items_to_be_indexed__[sender].append(instance)
-
-        if len(__items_to_be_indexed__[sender]) >= buffer_size:
-            update_index(__items_to_be_indexed__[sender], sender.__name__, buffer_size)
-            # Let's now empty this buffer or we'll end up reindexing every item which was previously buffered.
-            __items_to_be_indexed__[sender] = []
-        
-        raise RuntimeWarning('Handle save called.')
+        self.post_save_connector(sender, instance, **kwargs)
 
     def handle_delete(self, sender, instance, **kwargs):
-        try:
-            Bungiesearch.get_index(sender, via_class=True)
-        except KeyError:
-            return # This model is not managed by Bungiesearch.
-
-        delete_index_item(instance, sender.__name__)
-        raise RuntimeWarning('Handle delete called.')
+        self.pre_delete_connector(sender, instance, **kwargs)
 
     def setup(self, model):
         signals.post_save.connect(self.handle_save, sender=model)
