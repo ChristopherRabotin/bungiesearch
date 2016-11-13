0e64c870e86f9ac33ad19885f19cb0ab752a303d
Moemen Abd ElKader <moemenology@gmail.com>
2016-11-13 10:18:27 +0200

    Add TEMPLATES variable to tests settings.py. To work on Django 1.10

--- tests/settings.py
+++ tests/settings.py
@@ -28,6 +28,25 @@ DATABASES = {
     }
 }
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 BUNGIESEARCH = {
     'URLS': [os.getenv('ELASTIC_SEARCH_URL', 'localhost')],
     'ES_SETTINGS': {
